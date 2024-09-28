49b746f74a75054ca22d1849213afaa1803c3780
Dev Server <dennisocana@gmail.com>
2024-09-28 14:56:22 -0400

    First Project Configs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,26 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, provideExperimentalZonelessChangeDetection, isDevMode} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {provideHttpClient} from "@angular/common/http";
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    provideExperimentalZonelessChangeDetection(), provideHttpClient(), provideTransloco({
+        config: { 
+          availableLangs: ['(es', 'en)'],
+          defaultLang: '(es',
+          // Remove this option if your application doesn't support changing language in runtime.
+          reRenderOnLangChange: true,
+          prodMode: !isDevMode(),
+        },
+        loader: TranslocoHttpLoader
+      }),
+  ]
 };
