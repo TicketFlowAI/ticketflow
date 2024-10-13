6db64bfcad65ed6d8213d5a171b835fd055a1eba
Jose Miguel Merlo <josemielmer2002@hotmail.com>
2024-10-13 16:12:58 -0500

    Refactor(theme): the app now takes the system theme of the user and set it auto.

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -6,17 +6,44 @@ import {BehaviorSubject} from "rxjs";
   providedIn: 'root'
 })
 export class ThemeService {
+  //SERVICES
   private cookieService = inject(CookieService)
 
+  //PROPS N VARS
   private readonly THEME_KEY = 'theme'
   currentTheme$: BehaviorSubject<string> = new BehaviorSubject<string>(this.cookieService.get(this.THEME_KEY) || 'light');
 
+  //METHODS
   setStartTheme(){
+    if(this.cookieService.get(this.THEME_KEY) != ''){
+      this.getSavedTheme();
+    }
+    else
+      this.createTheme();
     document.documentElement.setAttribute('data-bs-theme', this.currentTheme$.getValue());
   }
+
   toggleTheme() {
     this.currentTheme$.next(this.currentTheme$.getValue() === 'light' ? 'dark' : 'light');
     document.documentElement.setAttribute('data-bs-theme', this.currentTheme$.getValue());
     this.cookieService.set(this.THEME_KEY, this.currentTheme$.getValue())
+  }
+
+  private getSavedTheme() {
+    const theme = this.cookieService.get(this.THEME_KEY);
+    this.currentTheme$.next(theme);
+  }
+
+  private createTheme() {
+    const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    let theme = '';
+
+    if(isDarkMode)
+      theme = 'dark';
+    else
+      theme = 'light';
+
+    this.cookieService.set(this.THEME_KEY, theme);
+    this.currentTheme$.next(theme);
   }
 }
